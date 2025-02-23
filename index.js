e99efd102c3420cd40687b4820d1107f7cb6d101
Mayankgup12 <mayankkumargupta1987@gmail.com>
2025-02-23 15:57:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from '@mui/material';
+import Button
+ from '@mui/material';
+<Button variant="contained">Click Me</Button>
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: blue;
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+`;
+
+<Button>Click Me</Button>
+import './App.css';
+    {Button} {
+    background: blue;
+    color: white;
+    padding: 10px;
+    border_radius: 5px;
+  }
+  
+
